ebd90f37bd875d6e54f39cba1e41b75b22b8d637
zd.yao <2674290996@qq.com>
2016-06-18 13:22:50 +0800

    添加项目文件。

---
+++ PerformanceTestTool2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PerformanceTestTool2
+{
+    class Program
+    {
+        //原子计数
+        public static int Count;
+        static void Main(string[] args)
+        {
+            Logger.Initialize(@"D:\temp-test", "log.txt");
+            PerformanceTest.Initialize();
+            //
+            PerformanceTest.Time("Test_Set", 400, 1000, (Test_Set));
+            Console.WriteLine("Test End");
+            Logger.Close();
+            Console.Read();
+        }
+
+        static void Test_Set()
+        {
+            //
+        }
+
+        static void Test_Get()
+        {
+            //
+        }
+
+        static void Example()
+        {
+            var current = Interlocked.Increment(ref Count);
+            Logger.Info(current.ToString());
+        }
+    }
+}
